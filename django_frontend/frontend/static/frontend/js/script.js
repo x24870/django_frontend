fb9864b0b3c231fed9489b7cb6aac458a343fa1f
pyk <x24870@gmail.com>
2020-10-19 23:59:29 +0800

    bind submit with task-create api, but without CSRF token this request will fail

--- django_frontend/frontend/static/frontend/js/script.js
+++ django_frontend/frontend/static/frontend/js/script.js
@@ -1,19 +1,43 @@
 window.addEventListener('load', () => {
   buildList();
+
+  //bind submit button listener
+  const form = document.getElementById('form-wrapper');
+  form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    console.log('form submitted');
+
+    let url = 'http://127.0.0.1:8000/api/task-create/';
+    let title = document.getElementById('title').value;
+
+    fetch(url, {
+      method: 'POST',
+      headers: {
+        'Content-type': 'application/json',
+      },
+      body: JSON.stringify({ 'title': title })
+    })
+      .then((response) => {//refresh task list after submitted
+        buildList()
+      })
+
+  });
+
 });
 
 function buildList() {
   const wrapper = document.getElementById('list-wrapper');
+  wrapper.innerHTML = '';//clear the wrapper
 
   let url = 'http://127.0.0.1:8000/api/task-list/';
 
   fetch(url)
     .then((resp) => resp.json())
-    .then((data)=>{
+    .then((data) => {
       console.log('Data:', data);
 
       let list = data;
-      for(let i in list){
+      for (let i in list) {
         let item = `
           <div id="data-row-${i}" class="task-wrapper flex-wrapper">
             <div style="flex:7">
@@ -32,4 +56,5 @@ function buildList() {
         wrapper.innerHTML += item;
       }
     });
-  }
+}
+
