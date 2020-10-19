aed62897f05abe30bf6912a316db6075ca77f30d
pyk <x24870@gmail.com>
2020-10-19 23:41:08 +0800

    fetch task list data

--- django_frontend/frontend/static/frontend/js/script.js
+++ django_frontend/frontend/static/frontend/js/script.js
@@ -10,6 +10,26 @@ function buildList() {
   fetch(url)
     .then((resp) => resp.json())
     .then((data)=>{
-      console.log('Data:', data)
-    })
+      console.log('Data:', data);
+
+      let list = data;
+      for(let i in list){
+        let item = `
+          <div id="data-row-${i}" class="task-wrapper flex-wrapper">
+            <div style="flex:7">
+              <span class="title">${list[i].title}</span>
+            </div>
+            <div style="flex:1">
+              <button class="btn btn-sm btn-outline-info edit">Edit</button>
+            </div>
+            <div style="flex:1">
+              <button class="btn btn-sm btn-outline-dark delete">-</button>
+            </div>
+          </div>
+        `;
+
+        //append to list-wrapper
+        wrapper.innerHTML += item;
+      }
+    });
   }
