4b2f6b526d3c6d1cd2bba3d61b05487ea88f999a
pyk <x24870@gmail.com>
2020-12-08 00:49:57 +0800

    add react frontend

--- django_frontend/django_frontend/settings.py
+++ django_frontend/django_frontend/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/3.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -59,7 +59,9 @@ ROOT_URLCONF = 'django_frontend.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            os.path.join(BASE_DIR, 'frontend_react', 'build')
+            ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -123,6 +125,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'frontend_react', 'build', 'static')
+]
 
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
