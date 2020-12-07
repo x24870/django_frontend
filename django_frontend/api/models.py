f16b9d49dd46c4f26b725436080a4ab524f59b1e
pyk <x24870@gmail.com>
2020-12-08 00:19:01 +0800

    correct typo

--- django_frontend/api/models.py
+++ django_frontend/api/models.py
@@ -4,7 +4,7 @@ from django.db import models
 
 class Task(models.Model):
     title = models.CharField(max_length=200)
-    complete = models.BooleanField(default=False, blank=True, null=True)
+    completed = models.BooleanField(default=False, blank=True, null=True)
 
     def __str__(self):
         return self.title
